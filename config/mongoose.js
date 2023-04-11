d2529ad0dddd77854993dbcb83dbeabb7473bda0
csmama29 <csmama29@gmail.com>
2023-04-11 11:56:59 +0530

    first commit

---
+++ config/mongoose.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+//require the library
+const mongoose = require('mongoose');
+
+// here we are using the MongoDB Url we defined in our ENV file
+console.log("thamitha",process.env.DB_URL);
+let DB_URL = process.env.DB_URL;
+
+//connect to the database
+mongoose.connect(DB_URL , 
+  {
+    usenewurlparser: true,
+    useunifiedtopology: true
+  }).then(()=>{
+    console.log(`connection successful `);
+  }).catch((err)=>{
+    console.log(`error connecting to database` , err);
+  })                                    
+  
+ 
+
+// acquire connection (to check if its successful)
+// const db = mongoose.connection;
+
+// db.on('error', console.e/rror.bind(console, 'Error connecting to MongoDB'));
+
+// db.once('open', function () {
+//   console.log('Connected to Database :: MongoDB');
+// });
+
+// module.exports = db;
